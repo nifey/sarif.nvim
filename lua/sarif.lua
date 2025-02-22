7ca4902b00659af4f834a67aa458917d836abcd1
Abdun Nihaal <abdun.nihaal@gmail.com>
2025-02-22 16:06:57 +0530

    Refactor to use OOPs
    - Defined classes for Result, Run and SarifLog
    - Changed the user commands to use the classes

--- lua/sarif.lua
+++ lua/sarif.lua
@@ -1,24 +1,110 @@
 local M = {}
 
+--[[
+--  Result class
+--]]
+local Result = {}
+Result.__index = Result
+Result.new = function(json)
+  local level = ""
+  if json["level"] == "error" then
+    level = "ERR  "
+  elseif json["level"] == "warning" then
+    level = "WARN "
+  elseif json["level"] == "note" then
+    level = "INFO "
+  end
+
+  return {
+    json = json,
+    level = level,
+    message = json["message"]["text"],
+  }
+end
+
+function Result:print(self)
+  -- FIXME handle other message types and placeholders
+  return self.level .. self.json["locations"][1]["physicalLocation"]["artifactLocation"]["uri"] .. " : ".. self.message
+end
+
+--[[
+--  Run class
+--]]
+local Run = {}
+Run.__index = Run
+Run.new = function(json)
+  local results = {}
+  -- Get a list of all results
+  for _, result in ipairs(json["results"]) do
+    table.insert(results, Result.new(result))
+  end
+
+  return {
+    json = json,
+    results = results,
+  }
+end
+
+function Run:get_results(self)
+    return self.results
+end
+
+--[[
+--  SarifLog class
+--]]
+local SarifLog = {}
+SarifLog.__index = SarifLog
+SarifLog.new = function(filename)
+  -- Load the file and decode as JSON
+  local ok, file_contents = pcall(vim.fn.readfile, filename)
+  if not ok then
+    vim.print("Error opening file " .. filename)
+    return
+  end
+  local ok, json = pcall(vim.fn.json_decode, file_contents)
+  if not ok then
+    vim.print("Error while loading file: " .. sarif_log)
+    return
+  end
+
+  -- Get a list of all runs
+  local runs = {}
+  for _, run in ipairs(json["runs"]) do
+    table.insert(runs, Run.new(run))
+  end
+
+  return {
+    filename = filename,
+    json = json,
+    runs = runs,
+  }
+end
+
+function SarifLog:get_results(self)
+  local results = {}
+  for _, run in ipairs(self.runs) do
+    for _, result in ipairs(Run:get_results(run)) do
+      table.insert(results, result)
+    end
+  end
+  return results
+end
+
+
+
+--[[
+--  Plugin State and Commands
+--]]
 local state = {
   sarif_logs = {}
 }
 
 M.load_sarif_file = function(opts)
   filename = opts[1]
-  local ok, file_contents = pcall(vim.fn.readfile, filename)
-  if not ok then
-    vim.print("Error opening file " .. filename)
-    return
-  end
-
-  local ok, sarif_log = pcall(vim.fn.json_decode, file_contents)
-  if not ok then
-    vim.print("Error while loading file: " .. sarif_log)
-    return
-  else
-    vim.print("Loaded SARIF log " .. filename .. " successfully")
+  local sarif_log = SarifLog.new(filename)
+  if sarif_log then
     state.sarif_logs[filename] = sarif_log
+    vim.print("Loaded SARIF log " .. filename .. " successfully")
   end
 end
 
@@ -44,26 +130,13 @@ M.view_sarif = function()
   vim.api.nvim_win_set_config(win, config)
 
   -- Parse SARIF logs and display a list of bugs to display
-  local bug_list = {}
+  local result_list = {}
   for _, sarif_log in pairs(state.sarif_logs) do
-    for _, run in ipairs(sarif_log["runs"]) do
-      for _, result in ipairs(run["results"]) do
-        local message = ""
-        if result["level"] == "error" then
-          message = "ERR  "
-        elseif result["level"] == "warning" then
-          message = "WARN "
-        elseif result["level"] == "note" then
-          message = "INFO "
-        end
-
-        message = message .. result["locations"][1]["physicalLocation"]["artifactLocation"]["uri"]
-        message = message .. " : ".. result["message"]["text"]
-        table.insert(bug_list, message)
-      end
+    for _, result in ipairs(SarifLog:get_results(sarif_log)) do
+      table.insert(result_list, Result:print(result))
     end
   end
-  vim.api.nvim_buf_set_lines(buf, 0, 0, false, bug_list)
+  vim.api.nvim_buf_set_lines(buf, 0, 0, false, result_list)
 
   -- Set some keybindings for the buffer
   buffer_keymap("q", buf, function() vim.api.nvim_win_close(win, true) end)
